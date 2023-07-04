1e67737da06db1edca08b533488e12c32978d03d
Libertas <1adamsvoboda@gmail.com>
2023-07-04 18:23:52 +0200

    updated about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,4 +7,7 @@
 	<h1>About Disinfect!</h1>
 
 	<p>This is a bug tracker for Word'n'Seek. Submit bugs you encounter while using the app.</p>
+
+	<h2>Used material</h2>
+	<p>This app uses <a href="https://fontawesome.com/">Font Awesome</a> icons.</p>
 </div>
